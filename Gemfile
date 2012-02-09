83830719ee47efa2dcadeba05086c98707d29782
ichord <chord.luo@gmail.com>
2012-02-09 14:22:39 +0800

    rspec

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
-source "http://rubygems.org"
+#source "http://rubygems.org"
+source "http://ruby.taobao.org"
 
 # Specify your gem's dependencies in jquery-atwho-rails.gemspec
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,24 @@
-# Set up RSpec
-%q{
-require 'webmock/rspec'
-
-RSpec.configure do |config|
-  config.include WebMock::API
-end
-}
 # Set up generator tests
+require 'rails'
 require 'rails/all'
 require 'rails/generators'
 
 class TestApp < Rails::Application
   config.root = File.dirname(__FILE__)
 end
+Rails.application = TestApp
 
+module Rails
+  def self.root
+    @root ||= File.expand_path("../../tmp/", __FILE__)
+  end
+end
+Rails.application.config.root = Rails.root
+#Rails.application.config.assets.enabled = true
+# Call configure to load the settings from
+# Rails.application.config.generators to Rails::Generators
+#Rails::Generators.configure!
+
+#require 'jquery-atwho-rails'
 require 'generators/jquery-atwho/install_generator'
 require 'generator_spec/test_case'
